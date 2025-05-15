05be66d010b96539878c5abeeba0306d649f0f82
Asesa Bandhudasz <asesabandhudasz@Asesa-MacBook-Pro.local>
2025-05-15 16:50:22 +0200

    fix: render 5050 port és requests javítás

--- app.py
+++ app.py
@@ -51,4 +51,6 @@ def webhook():
 import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    import os
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host="0.0.0.0", port=port)
