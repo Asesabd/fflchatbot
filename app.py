cc607cc6f5888d6fab00061e86aade3e3f3113b0
Asesa Bandhudasz <asesabandhudasz@Asesa-MacBook-Pro.local>
2025-05-15 18:15:46 +0200

    fix: CORS engedélyezve karitativ.hu számára

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
+from flask_cors import CORS
 import requests
 from chatbot import valaszolo_bot
 
 app = Flask(__name__)
+CORS(app, origins=["https://karitativ.hu"])
 
 import os
 VERIFY_TOKEN = os.environ.get("VERIFY_TOKEN", "lokalis_token")
